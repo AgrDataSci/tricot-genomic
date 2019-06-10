7f2e63800c7bf5f274e9ee836f5cfd66a9766e52
kauedesousa <desousa.kaue@gmail.com>
2019-06-10 11:17:59 +0200

    modis_chirps

--- script/02_add_climate_indices.R
+++ script/02_add_climate_indices.R
@@ -14,6 +14,9 @@ df <- "data/durumwheat.csv"
 df %<>% 
   read_csv()
 
+df %<>%
+  distinct(id, .keep_all = TRUE)
+
 n <- nrow(df)
 
 # define some parameters
@@ -31,12 +34,12 @@ k_to_c <- 273.15
 
 tiles <- c("h21v07","h21v08")
 
+years <- c(2012:2016)
+
 dates <- NULL
 
-summary(df$planting_date)
-
 # generate a sequency of 8-days from 2013 to 2015
-for(i in c(2003:2015)){
+for(i in c(2012:2015)){
   dates <- c(dates, c(seq(as.Date(paste0(i, "-01-01"), "%Y-%m-%d"), 
                           as.Date(paste0(i, "-12-31"), "%Y-%m-%d"), 8)))
 }
@@ -62,10 +65,6 @@ modis <- array(NA, dim = c(n, length(dates), 2),
 # they are also very large so it is retrieved from a external drive
 # check if the drive exists
 
-if (!exists(edrive)) {
-  stop("An external drive with MODIS data is not available")
-}
-
 for (i in 1:2){
   
   if(i == 1 ) m <- "day" else m <- "night"
@@ -75,6 +74,10 @@ for (i in 1:2){
     file <- list.files(paste0(edrive, "modis/smooth_Ethiopia/", tiles[j]), 
                        pattern = m, 
                        full.names = TRUE)
+    
+    
+    file <- file[grepl(paste0(years, collapse = "|"), file)]
+    
     
     file <- stack(file)
     
@@ -110,45 +113,38 @@ for (i in 1:dim(modis)[2]) {
 
 # apply linear interpolation to the 8-day temperature
 modis_approx <- array(NA, c(n, length(full_dates), 2), 
-                      dimnames = list(mydata$id,  as.character(full_dates), c(1:2) ))
+                      dimnames = list(df$id,  as.character(full_dates), c(1:2) ))
 
 for(i in 1:dim(modis)[1]){
   modis_approx[i,,1] <- approx(modis[i,,1], n = length(full_dates))[[2]]
   modis_approx[i,,2] <- approx(modis[i,,2], n = length(full_dates))[[2]]
 }
-#save(modis, modis_approx, file = "./processing/modis_ethiopia.RData")
-load("./processing/modis_ethiopia.RData")
-
+#save(modis, modis_approx, file = "data/modis.rda")
 
 # #add CHIRPS data
 cat("Readind CHIRPS data. Time:", date(), "\n")
-# #source CHIRPS (precipitation data)
-# #http://chg.geog.ucsb.edu/data/chirps/
-# years <- years <- seq(from=2003 , to=2017, by=1)
-# chirps <- list()
-# #run CHIRPS over the chosen years
-# for (i in years){
-#   print(i)
-#   info <- raster::stack(list.files(paste0("E:/rasters/CHIRPS/", i), pattern = ".tif", full.names = T))
-#   names(info) <- gsub("chirps.v2.0.", "d", names(info))
-#   chirps[paste("y", i ,sep="")] <- info
-#   rm(info)
-# }
-# #convert list into one large raster stack
-# chirps <- raster::stack(chirps)
-# #chirps <- raster::crop(chirps, my_ext)
-# #extract precipitation info using lat and lon of each farm
-# chirps <- raster::extract(chirps, mydata[,c("lon","lat")])
-# sum(is.na(chirps))
-# dimnames(chirps)[[2]] <- gsub("d", "", dimnames(chirps)[[2]])
-# dimnames(chirps)[[2]] <- gsub("[.]", "-", dimnames(chirps)[[2]])
-# save(chirps, file = "./processing/chirps_ethiopia.RData")
-load("./processing/chirps_ethiopia.RData")
+#source CHIRPS (precipitation data)
+#http://chg.geog.ucsb.edu/data/chirps/
+years <- c(2013:2016)
+chirps <- list()
+#run CHIRPS over the chosen years
+for (i in years){
+  print(i)
+  info <- raster::stack(list.files(paste0(edrive, "CHIRPS/", i), 
+                                   pattern = ".tif", full.names = TRUE))
+  names(info) <- gsub("chirps.v2.0.", "d", names(info))
+  chirps[paste("y", i ,sep="")] <- info
+  rm(info)
+}
+#convert list into one large raster stack
+chirps <- raster::stack(chirps)
+#chirps <- raster::crop(chirps, my_ext)
+#extract precipitation info using lat and lon of each farm
+chirps <- raster::extract(chirps, df[,c("lon","lat")])
+sum(is.na(chirps))
+dimnames(chirps)[[2]] <- gsub("d", "", dimnames(chirps)[[2]])
+dimnames(chirps)[[2]] <- gsub("[.]", "-", dimnames(chirps)[[2]])
+save(chirps, file = "data/chirps.rda")
 
 
-n <- nrow(mydata)
-
-#add season 
-mydata$season <- paste("Meher", mydata$Year, sep="-")
-mydata$season <- gsub("20","",mydata$season)
 
