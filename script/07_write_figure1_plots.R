004b1e6ac823ad1e50a48eb6493b718ed391d88e
kauedesousa <kaue.desousa@inn.no>
2020-04-15 12:56:16 +0200

    fig1

--- script/07_write_figure1_plots.R
+++ script/07_write_figure1_plots.R
@@ -3,12 +3,15 @@
 #...............................................
 library("raster")
 library("sf")
+library("smoothr")
+library("dismo")
 library("ggplot2")
 
 #...............................................
 #...............................................
 # define the extent 
-e <- extent(-86, -85.3, 12.8, 13.42)
+#e <- extent(-86, -85.3, 12.8, 13.42)
+e <- extent(-85.95, -85.3, 12.8, 13.41)
 
 #...............................................
 #...............................................
@@ -23,7 +26,8 @@ l <- st_as_sf(l)
 
 l$NAME_1 <- c("Var A", "Var A", "Var B")
 
-colors <- c("#ef8a62", "#2166ac","#fee090","#67a9cf")
+colors <- c("#253dac","#70e072")
+
 p <- ggplot(l) +
   geom_sf(aes(fill = as.factor(NAME_1)),
           lwd = 0) + 
@@ -32,7 +36,7 @@ p <- ggplot(l) +
   theme_void() +
   theme(legend.position="bottom",
         legend.title = element_blank(),
-        legend.text= element_text(size = 20, colour="black"))
+        legend.text= element_text(size = 15, colour="black"))
 
 p
 
@@ -47,22 +51,31 @@ ggsave("output/fig1/centralized.svg",
 # this file is an illustration for the idea of decentralized approach
 # recommendations based on environmental conditions
 d <- raster("data/fig1.tif")
-
+plot(d)
+plot(e, add = TRUE)
 d <- crop(d, e)
 
 plot(d)
 
 
 # replace some values
-extf <- extent(c(-86,-85.65,12.8,13.14))
-extf2 <- extent(c(-85.8,-85.42,13.25,13.42))
+extf <- extent(c(-85.95,-85.65,12.8,13.14))
+extf2 <- extent(c(-85.8,-85.42,13.25,13.41))
 extf3 <- extent(c(-85.5,-85.40,13.13,13.25))
 extf4 <- extent(c(-85.7,-85.41,12.8,12.9))
+extf5 <- extent(c(-85.55,-85.38,13,13.12))
+extf6 <- extent(c(-85.8,-85.68,12.8,12.88))
+extf7 <- extent(c(-85.5,-85.43,13.22,13.3))
+
 plot(d)
-plot(extf, add = TRUE)
-plot(extf2, add = TRUE)
-plot(extf3, add = TRUE)
-plot(extf4, add = TRUE)
+#plot(extf7, add = TRUE)
+plot(extf6, add = TRUE)
+plot(extf5, add = TRUE, col = "red")
+plot(extf, add = TRUE, col = "grey")
+plot(extf2, add = TRUE, col = "black")
+plot(extf3, add = TRUE, col = "pink")
+plot(extf4, add = TRUE, col = "green")
+
 # Change values
 d[extf][d[extf] == 3] <- 4
 d[extf][d[extf] == 1] <- 4
@@ -72,36 +85,58 @@ d[extf2][d[extf2] == 2] <- 1
 d[extf3][d[extf3] == 1] <- 2
 d[extf3][d[extf3] == 3] <- 2
 
-d[extf4][d[extf4] == 1] <- 4
-d[extf4][d[extf4] == 3] <- 4
+d[extf4][d[extf4] == 1] <- 2
+d[extf4][d[extf4] == 3] <- 2
+
+d[extf5][d[extf5] == 3] <- 2
+
+d[extf6][d[extf6] == 2] <- 4
+
+
+d4 <- as.data.frame(d, xy = TRUE)
+d4 <- d4[d4$fig1==4,]
+d4 <- d4[d4$y > 12.85,]
+d4 <- d4[d4$x > -85.84,]
+d4 <- dismo::convHull(d4)
+d4 <- d4@polygons
+r <- raster(d)
+d4 <- rasterize(d4, r)
+
+plot(d)
+plot(d4, add = TRUE)
+
+
+d[d4][d[d4] !=10] <- 4
 
 plot(d)
 
-d <- as.data.frame(d, xy = TRUE)
+s <- rasterToPolygons(d, dissolve = TRUE)
+s <- st_as_sf(s)
+names(s)[1] <- "value"
+s <- smooth(s, method = "chaikin", refinements = 1)
+s <- smooth(s, method = "ksmooth", smoothness = 1)
+plot(s)
 
-unique(d[,3])
-
-summary(as.factor(d[,3]))
-
-d <- d[!is.na(d$fig1), ]
-
-head(d)
-
-colors <- c("#ef8a62", "#2166ac","#fee090","#67a9cf")
+colors <- c("#253dac",
+            "#70e072",
+            "#f1c73e",
+            "#2a4d0c")
+#colors <- sample(colors, 4)
 
 p2 <- 
-  ggplot(d) + 
-  geom_raster(aes(y = y, x = x, 
-                  fill = factor(fig1, levels = c("1","4","3","2")))) +
+  ggplot(s) + 
+  geom_sf(aes(fill = factor(value, levels = c("1","4","3","2"))),
+          lwd = 0) +
   scale_fill_manual(values= colors,
                     name = NULL,
                     labels = c("Var A", "Var B", "Var C", "Var D")) +
   theme_void() +
   theme(legend.position="bottom",
         legend.title = element_blank(),
-        legend.text= element_text(size = 20, colour="black"))
+        legend.text= element_text(size = 15, colour="black"))
 
 p2
+
 ggsave("output/fig1/decentralized.svg", 
        plot = p2,
        width = 11,
