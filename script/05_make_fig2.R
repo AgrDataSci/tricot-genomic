7a36e44d0f133694ca9d8cfe607ccd21c3891ed4
kauedesousa <kaue.desousa@inn.no>
2020-04-30 13:00:51 +0200

    fig-2

--- script/05_make_fig2.R
+++ script/05_make_fig2.R
@@ -119,10 +119,11 @@ wp <- cbind(node = nodes, wp)
 wp <- wp[!duplicated(wp$node), ]
 wp <- t(wp)
 wp <- as.data.frame(wp, stringsAsFactors = FALSE)
-names(wp) <- gsub(" ","",paste0("worth_",wp[1,]))
+names(wp) <- gsub(" ","",paste0("worth_", wp[1,]))
 wp$genotype <- rownames(wp)
 wp <- wp[-1,]
 wp[,1:3] <- lapply(wp[,1:3], as.numeric)
+head(wp)
 
 # get the reference variety from each plot
 R <- dt$G
@@ -168,13 +169,18 @@ for(i in seq_len(n)) {
 
 head(rel)
 
+str(rel)
+
 table(rel$ref)
 table(rel$gen)
 
-node_sel <- cbind(table(rel$gen[rel$node=="node 3"]),
-                  table(rel$gen[rel$node=="node 4"]),
-                  table(rel$gen[rel$node=="node 5"]))
-dimnames(node_sel)
+table(rel$gen[rel$node=="node 3"])
+table(rel$gen[rel$node=="node 4"])
+table(rel$gen[rel$node=="node 5"])
+
+node4 <- c("048ET_D","060ET_D","169ET_D")##"415ET_D")
+node3 <- table(rel$gen[rel$node=="node 3"])
+node3 <- names(node3)[!names(node3) %in% node4]
 
 rel <- rel[rel$best < 4, ]
 rel$best <- as.factor(ifelse(rel$best == 1, "1st",
@@ -182,9 +188,10 @@ rel$best <- as.factor(ifelse(rel$best == 1, "1st",
                                     "2nd", "3rd")))
 
 p2 <- ggplot(rel,
-             aes(y = reliability, group = best, 
+             aes(y = reliability, 
+                 group = best, 
                  x = best, fill = best)) +
-  geom_boxplot(outlier.size = 0.5, show.legend = FALSE) +
+  geom_boxplot(outlier.size = 0.5, show.legend = FALSE, colour = "grey30") +
   scale_fill_manual(values= c("#2166ac","#6baed6","#9ecae1")) + 
   labs(y = "Probability of outperforming",
        x = c(""),
@@ -192,19 +199,20 @@ p2 <- ggplot(rel,
   scale_y_continuous(limits = c(.8, .95),
                      breaks = seq(8, 10, 0.5)/10) +
   theme(axis.text.x = element_text(size=12, angle = 0,
-                                   face="plain", colour = "black"),
+                                   face="plain", colour = "grey30"),
         axis.text.y = element_text(size=12, angle = 0,
                                    hjust=1, vjust=0.5,
-                                   face="plain", colour = "black"),
-        axis.title.y = element_text(size=12, colour = "black"),
-        axis.line = element_line(colour = "black"),
+                                   face="plain", colour = "grey30"),
+        axis.title.y = element_text(size=12, colour = "grey30"),
+        axis.line = element_line(colour = "grey30"),
         plot.background = element_blank(),
         panel.background = element_blank(),
         panel.border = element_rect(linetype = "solid",
-                                    fill = NA),
+                                    fill = NA,
+                                    colour = "grey30"),
         plot.margin = unit(c(3,3,1,3), "mm"),
         plot.title = element_text(size=16, 
-                                  colour = "black", 
+                                  colour = "grey30", 
                                   face = "bold"),
         text = element_text(family = "sans"))
 
@@ -217,9 +225,10 @@ p2
 nodepca <- read.csv("output/reliability_yield_gain/node_gen.csv")
 dt <- nodepca
 
-nodesel <- data.frame(cbind(genotype = dimnames(node_sel)[[1]],
-                 group = c(rep("3DB Cold tolerant", 3), rep("3DB Warm tolerant", 3))),
-           stringsAsFactors = FALSE)
+nodesel <- data.frame(genotype = c(node3, node4),
+                      group = c(rep("3DB Cold tolerant", 3), rep("3DB Warm tolerant", 3)))
+
+str(nodesel)
 
 pca <- merge(nodepca, nodesel, by = "genotype", all.x = TRUE)
 pca$group <- ifelse(pca$PC1 < - 100, "Currently recommended", pca$group) 
@@ -229,40 +238,127 @@ pca$group <- factor(pca$group, levels = c("3DB Cold tolerant","3DB Warm tolerant
 
 pca <- merge(pca, wp, by = "genotype")
 
+pca$mean_worth <- rowMeans(pca[,paste0("worth_node", 3:5)])
 
-p1 <-
+p1 <- 
 ggplot() +
-  geom_point(data = pca, 
-             aes(x = PC1, y = PC2, fill = group, 
-                 color = group, shape = group), size = 1.5) +
-  scale_shape_manual(values = c(15, 16, 17, 5)) +
-  scale_color_manual(values= c("#91c43e","#9f49c3","#e0493d","gray40")) +
+  geom_point(data = pca,
+             aes(x = PC1, y = PC2, color = mean_worth),
+             shape = 19, size = 4) +
+  scale_color_gradientn(colours = c("gray70", "gray30"),
+                        breaks = c(min(pca$mean_worth), max(pca$mean_worth)),
+                        labels = c("Low", "High"),
+                        name = "Overall appreciation") +
+  geom_point(data = pca[pca$group=="Currently recommended",],
+             aes(x = PC1, y = PC2), col = "#FF00FF", size = 2) +
+  geom_point(data = pca[pca$group=="3DB Cold tolerant",],
+             aes(x = PC1, y = PC2), col = "#225ea8", size = 2) +
+  geom_point(data = pca[pca$group=="3DB Warm tolerant",],
+             aes(x = PC1, y = PC2), col = "#f03b20", size = 2) +
   labs(x = "PC1 (24.1%)",
        y = "PC2 (10.5%)",
        title = "A") +
-  theme_bw() +
   theme(axis.text.x = element_text(size = 12, angle = 0,
-                                   face = "plain", colour = "black"),
+                                   face = "plain", colour = "grey30"),
         axis.text.y = element_text(size = 12, angle = 0,
                                    hjust = 1, vjust = 0.5,
-                                   face = "plain", colour = "black"),
-        axis.title.y = element_text(size = 12, colour = "black"),
+                                   face = "plain", colour = "grey30"),
+        axis.title.y = element_text(size = 12, colour = "grey30"),
+        axis.title.x = element_text(size = 12, colour = "grey30"),
         panel.grid.minor = element_blank(),
-        legend.text = element_text(size = 10),
-        legend.title = element_blank(),
-        legend.position = c(0.25,0.9),
+        panel.background = element_blank(),
+        legend.text = element_text(size = 10, color = "grey30"),
+        legend.title = element_text(size = 11, face = "bold", colour = "grey30"),
+        legend.position = c(0.22,0.78),
         legend.background = element_blank(),
         legend.key.size = unit(0.3, "cm"),
+        legend.box.background = element_blank(),
+        legend.key = element_blank(),
         panel.grid.major = element_blank(),
-        panel.border = element_rect(linetype = "solid",
-                                    fill = NA),
+        panel.border = element_rect(linetype = "solid", fill = NA, color = "grey30"),
         plot.margin = unit(c(3,3,1,3), "mm"),
         plot.title = element_text(size = 16, 
-                                  colour = "black", 
+                                  colour = "grey30", 
                                   face = "bold"),
         text = element_text(family = "sans"))
 
 p1
+
+p11 <- 
+ggplot() +
+  geom_point(data = pca,
+             aes(x = PC1, y = PC2, color = group, shape = group), shape = 19, size = 3) +
+  scale_color_manual(values= c("#225ea8","#f03b20","#FF00FF","gray80"),
+                               name = "Genotype") +
+  labs(x = "PC1 (24.1%)",
+       y = "PC2 (10.5%)",
+       title = "A") +
+  theme(axis.text.x = element_text(size = 12, angle = 0,
+                                   face = "plain", colour = "grey30"),
+        axis.text.y = element_text(size = 12, angle = 0,
+                                   hjust = 1, vjust = 0.5,
+                                   face = "plain", colour = "grey30"),
+        axis.title.y = element_text(size = 12, colour = "grey30"),
+        axis.title.x = element_text(size = 12, colour = "grey30"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        legend.text = element_text(size = 10, color = "grey30"),
+        legend.title = element_text(size = 11, face = "bold", colour = "grey30"),
+        legend.position = c(0.22,0.78),
+        legend.background = element_blank(),
+        legend.key.size = unit(0.3, "cm"),
+        legend.box.background = element_blank(),
+        legend.key = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_rect(linetype = "solid", fill = NA, color = "grey30"),
+        plot.margin = unit(c(3,3,1,3), "mm"),
+        plot.title = element_text(size = 16, 
+                                  colour = "grey30", 
+                                  face = "bold"),
+        text = element_text(family = "sans"))
+
+p11
+
+
+ggplot() +
+  geom_point(data = pca,
+             aes(x = PC1, y = PC2, fill = mean_worth),
+             shape = 21, size = 4) +
+  scale_fill_gradientn(colors = c("gray70", "gray30"),
+                       breaks = c(min(pca$mean_worth), max(pca$mean_worth)),
+                       labels = c("Low", "High"),
+                       name = "Overall appreciation") +
+  geom_point(data = pca, 
+             aes(x = PC1, y = PC2, color = group), shape = 21, size = 4) +
+  scale_color_manual(values= c("#225ea8","#f03b20","#FF00FF","grey50"),
+                    name = "Genotype") +
+  labs(x = "PC1 (24.1%)",
+       y = "PC2 (10.5%)",
+       title = "A") +
+  theme(axis.text.x = element_text(size = 12, angle = 0,
+                                   face = "plain", colour = "grey30"),
+        axis.text.y = element_text(size = 12, angle = 0,
+                                   hjust = 1, vjust = 0.5,
+                                   face = "plain", colour = "grey30"),
+        axis.title.y = element_text(size = 12, colour = "grey30"),
+        axis.title.x = element_text(size = 12, colour = "grey30"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        legend.text = element_text(size = 10, color = "grey30"),
+        legend.title = element_text(size = 11, face = "bold", colour = "grey30"),
+        legend.position = c(0.22,0.75),
+        legend.background = element_blank(),
+        legend.key.size = unit(0.3, "cm"),
+        legend.box.background = element_blank(),
+        legend.key = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_rect(linetype = "solid", fill = NA, color = "grey30"),
+        plot.margin = unit(c(3,3,1,3), "mm"),
+        plot.title = element_text(size = 16, 
+                                  colour = "grey30", 
+                                  face = "bold"),
+        text = element_text(family = "sans"))
+
 # .................................
 # Panel C, Bar plot over the time series ####
 # # Gain 
@@ -332,9 +428,9 @@ p3 <-
   geom_errorbar(aes(ymin = se_min, ymax = se_max), 
                 width = 0.1,
                 position = position_dodge(1)) +
-  scale_x_discrete(breaks = seq(1,15,2)) +
+  #scale_x_discrete(breaks = seq(1,15,2)) +
   scale_y_continuous(limits = c(0, 0.35),
-                     breaks = seq(0, 3.5, 0.5)/10,
+                     breaks = seq(0, 3, 1)/10,
                      expand = c(0, 0)) +
   labs(y = "Increase in yield (%)",
        x = "Year",
@@ -342,45 +438,54 @@ p3 <-
   scale_fill_manual(values = c("#3182bd"), 
                       name = "") +
   theme(axis.text.x = element_text(size = 12, angle = 0,
-                                   face="plain", colour = "black"),
-        axis.title.x = element_text(size=12, colour = "black"),
+                                   face="plain", colour = "grey30"),
+        axis.title.x = element_text(size=12, colour = "grey30"),
         axis.text.y = element_text(size=12, angle = 0,
                                    hjust=1, vjust=0.5,
-                                   face="plain", colour = "black"),
-        axis.title.y = element_text(size=12, colour = "black"),
-        axis.line = element_line(colour = "black"),
-        legend.text = element_text(size=12, colour="black"),
+                                   face="plain", colour = "grey30"),
+        axis.title.y = element_text(size=12, colour = "grey30"),
+        axis.line = element_line(colour = "grey30"),
+        legend.text = element_text(size=12, colour="grey30"),
         legend.key = element_rect(colour = NA, fill = NA ,
                                   size=0.5,linetype = 1),
         legend.position = "none",
         plot.background = element_blank(),
         panel.background = element_blank(),
         panel.border = element_rect(linetype = "solid",
-                                    fill = NA),
+                                    fill = NA,
+                                    colour = "grey30"),
         plot.margin = unit(c(2,3,3,3), "mm"),
         plot.title = element_text(size=16, 
-                                  colour = "black", 
+                                  colour = "grey30", 
                                   face = "bold"),
         text = element_text(family = "sans"))
 
 p3
 
-p <- p1 + (p2 / p3) + plot_layout(ncol = 2, widths = c(2, 1))
+pf <- p12 + (p2 / p3) + plot_layout(ncol = 2, widths = c(2, 1))
 
-p
+
+
+pf2 <- p11 + (p2 / p3) + plot_layout(ncol = 2, widths = c(2, 1))
 
 # save as svg
 ggsave(paste0("manuscript/display_items/", "Fig2.svg"),
-       plot = p,
-       width = 20,
-       height = 15,
+       plot = pf,
+       width = 21,
+       height = 14,
        units = "cm")
+
+# save as svg
+ggsave(paste0("manuscript/display_items/", "Fig2_2.svg"),
+       plot = pf2,
+       width = 21,
+       height = 14,
+       units = "cm")
+
 
 # save as png
-ggsave(paste0(paste0("manuscript/display_items/", "Fig2.png")),
-       plot = p,
-       width = 20,
-       height = 15,
-       dpi = 500,
+ggsave(paste0("manuscript/display_items/", "Fig2.png"),
+       plot = pf,
+       width = 21,
+       height = 14,
        units = "cm")
-
